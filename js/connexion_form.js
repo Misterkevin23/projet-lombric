573d80e48684a175a9ad48068957aab990ecdbee
Mister kevin <pascal.d.kevin@gmail.com>
2017-05-11 19:11:36 +0200

    jquery

--- js/connexion_form.js
+++ js/connexion_form.js
@@ -17,25 +17,36 @@ function connexion_form(){
 }
 
 				
-$(":input").ready(function(){
+$(document).ready(function(){
 	$(".inscription_cadre").hide();
 	$(".connexion_cadre").show();
 	$(".inscrit").click(function(){
-		$(".inscription_cadre").hide();
-		$(".connexion_cadre").show();
+		$(".inscription_cadre").hide("slow");
+		$(".connexion_cadre").show("slow");
 	})
 	$(".non-inscrit").click(function(){
-		$(".inscription_cadre").show();
-		$(".connexion_cadre").hide();
+		$(".inscription_cadre").show("slow");
+		$(".connexion_cadre").hide("slow");
 	})
+})
 
+//$(document).ready(function(){
 	$(".panier_ajout").click(function(){
+		
+		$(".panier_ajout").attr("value" ,"Annuler");
+		if($(".panier_ajout").val()=="Ajouter au panier"){ 
 		parseFloat($(".nbre_article").text())+1;
 
-	// if($(".panier_ajout").attr("value")=="Ajouter au panier"){
-	// 	$(".panier_ajout").attr("value","Annuler");
-	// 	}
-	})	
+		}
+			
+		else{
+			$(".panier_ajout").attr("value","Ajouter au panier");
+			parseFloat($(".nbre_article").text())-1;
+		}
+	})
+//})
+
+
 	// $("main").mousemove(function(){
 	// 	(100*parseFloat($("article").css("margin-right")))/$("main").width();
 	// })
@@ -44,11 +55,28 @@ $(":input").ready(function(){
 	// 	if($(".panier_ajout").attr("value")=="Annuler"){
 	// 		$(".panier_ajout").attr("value","Ajouter au panier");
 	// 	}
-	// })	
+	// })
+$(document).ready(function(){
+
+		$("#menu ul li").hide()
+		$("#menu ul ul").hide()
+		$("#menu ul li").height("60px")
+		$("#menu ul ul").css({"left": "188px", "top" : "-50px"})
+		
+		$("#menu li").mousemove(function(){
+			$("#menu ul li").show()
+		})
+		$("#menu li").mouseout(function(){
+			$("#menu ul li").hide()
+		})
+		$("#menu ul li").mousemove(function(){
+			$("#menu ul ul").show()
+		})
+		$("#menu ul li").mouseout(function(){
+			$("#menu ul ul").hide()
+		})
 })
-
-
-
-
-
-	
+		
+		// $("#main li").mousemove(function(){
+		// 	$("#menu ul li").show()
+		// })
